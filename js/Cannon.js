b19779be96aecbe78ffa93b952a0c41f69639e57
memonisha <75033521+memonisha@users.noreply.github.com>
2021-09-01 07:56:23 +0530

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,13 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    
+  }
+
+  
+  
+}
